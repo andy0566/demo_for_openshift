fb8540acea5c60f57c16307a2c646f59d3629708
andy0566 <andy0566@163.com>
2022-07-19 15:57:04 +0200

    update1

--- src/main/java/com/example/demo_for_openshift/DemoForOpenshiftApplication.java
+++ src/main/java/com/example/demo_for_openshift/DemoForOpenshiftApplication.java
@@ -2,12 +2,20 @@ package com.example.demo_for_openshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoForOpenshiftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoForOpenshiftApplication.class, args);
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForOpenshiftApplication.class, args);
+    }
 
 }
