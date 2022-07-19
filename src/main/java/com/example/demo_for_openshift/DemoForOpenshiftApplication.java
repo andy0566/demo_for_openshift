5ebb91a2e7b50ebd408af4487d3cc1b64c0ab9e7
andy0566 <andy0566@163.com>
2022-07-19 15:17:01 +0200

    Initial commit

---
+++ src/main/java/com/example/demo_for_openshift/DemoForOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_for_openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForOpenshiftApplication.class, args);
+	}
+
+}
